6279d285cdd79cd24872b5e93e3912df6b75dbf9
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-01-13 22:48:06 -0500

    testing some things with bitmap, getting basic program structure down

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include <SDL.h>
 
+// Initializes SDL and creates a window.
+bool init();
+
+// Loads media to window
+bool loadMedia();
+
+// Frees media and closes SDL, releasing all associated memory
+void close();
+
+SDL_Window* window = NULL;
+SDL_Surface* screenSurface = NULL;
+SDL_Surface* imageToLoad = NULL;
+
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main(int argc, char* args[]) {
-  SDL_Window* window = NULL;
-  SDL_Surface* surface = NULL;
 
+bool init() {
+  bool success = true;
   if (SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
     printf("SDL could not initialize, SDL_ERROR: %s\n", SDL_GetError());
   }
@@ -17,23 +29,54 @@ int main(int argc, char* args[]) {
       printf("Window could not be created, SDL_ERROR: %s\n", SDL_GetError());
     }
     else {
-      surface = SDL_GetWindowSurface(window);
-      // Fills the surface white.
-      SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+      screenSurface = SDL_GetWindowSurface(window);
+    }
+  }
+  return success;
+}
+
+bool loadMedia() {
+  bool success = true;
+  imageToLoad = SDL_LoadBMP( "assets/test.bmp" );
+  if (imageToLoad == NULL) {
+    success = false;
+  }
+  return success;
+}
+
+void close() {
+  SDL_FreeSurface(imageToLoad);
+  imageToLoad = NULL;
+
+  SDL_DestroyWindow(window);
+  window = NULL;
+
+  SDL_Quit();
+}
+
+
+int main(int argc, char* args[]) {
+  if (!init()) {
+    printf("Failure to initialize window\n");
+  }
+  else {
+    if (!loadMedia()) {
+      printf("Failure to load media\n");
+    }
+    else {
+      SDL_BlitSurface(imageToLoad, NULL, screenSurface, NULL);
       SDL_UpdateWindowSurface(window);
-      SDL_Event e; 
+      SDL_Event e;
       bool quit = false;
-      while(quit == false){
-        while(SDL_PollEvent(&e)) {
-          if(e.type == SDL_QUIT) { 
+      while (quit == false) {
+        while (SDL_PollEvent(&e)) {
+          if (e.type == SDL_QUIT) {
             quit = true;
           }
         }
       }
-      SDL_DestroyWindow(window);
-      SDL_Quit();
+      close();
       return 0;
     }
-    
   }
 }
